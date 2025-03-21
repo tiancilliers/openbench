ae9e535f681376d0f8c8e2e41b6f7f9de60ceee4
Tian Cilliers <tiancilliers@gmail.com>
2025-03-21 13:18:42 +0200

    Added stable score calculation to Python script, and auto scaling factor calculation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# openbench
-Open source PC component benchmark repository and processing software
+# OpenBench
+OpenBench is an open-source PC component benchmark repository and processing software.
+
+## How it works
+OpenBench takes as input a set of benchmarks, each giving some performance metric of some subset of models. Since this performance metric may be different, it is only assumed to be directly correlated to the real performance through some constant factor. OpenBench's algorithm then produces a relative ranking of models present in one or more benchmarks, as well as a set of these constant factors, such that the ranking minimizes the mean squared error (MSE) between the performance numbers multiplied by the constant factors and the output ranking. **This basically allows the relative scoring of all models in each benchmark to contribute to the overall ranking in a mathematically optimal manner**. 
+
+OpenBench then scales the relative ranking of all models in a way that minimizes the change of existing models when new benchmarks are added. While slightly arbitrary, this allows the use of the BenchScore as a performance number that should not change as new, higher-performing, models are added. The vision for this is that retailers can use this database to provide accurate and stable performance estimates to their customers.
+
+### TLDR
+* The ratio between scores of different models is as accurate as possible
+* The absolute scores change as little as possible when new benchmarks are added
+
+## How to use
+Access the OpenBench rankings [here](https://tiancilliers.github.io/openbench).
+
+For more advanced use, use the `openbench.py` program. This contains the same algorithm, and a usage guide will be added in the future.
+
+## How to contribute
+Add benchmark results to the `data/` directory, following the existing formatting, and submit a pull request.
+
+> [!IMPORTANT]  
+> Do not modify the `data_list.json` file manually. This file contains the parameters needed for the correct BenchScore scaling, and needs to be updated via the correct algorithm to avoid scores fluctuating.
--- openbench.py
+++ openbench.py
@@ -1,7 +1,8 @@
 import numpy as np
 import json
+import os
 
-def rank_relative(benchmarks, unity):
+def rank_relative(benchmarks):
     models = list(set(model for bench in benchmarks for model in bench['models']))
     benchmark_sum_sq = [sum(result**2 for result in bench['results']) for bench in benchmarks]
     benchmark_dicts = [{model:result for model,result in zip(bench['models'],bench['results'])} for bench in benchmarks]
@@ -14,20 +15,69 @@ def rank_relative(benchmarks, unity):
             else:
                 solve_matr[i][j] = sum((benchmark_matr[i][k]**2/benchmark_sum_sq[k]-1)*benchmark_matr[i][k]*benchmark_matr[j][k]/benchmark_sum_sq[k] for k in range(len(benchmarks)) if benchmark_matr[i][k] is not None and benchmark_matr[j][k] is not None)
     u, e, v = np.linalg.svd(np.array(solve_matr))
-    solve_res = v[-1,:]/v[-1,models.index(unity)]
-    return {model:res for model,res in zip(models, solve_res)}
+    benchmark_ks, k_n, k_d = [], 0, 0
+    for i in range(len(benchmarks)):
+        bench_k_n, bench_k_d = 0, 0
+        for j in range(len(models)):
+            if benchmark_matr[j][i] is not None:
+                bench_k_n += benchmark_matr[j][i]*v[-1,j]
+                bench_k_d += benchmark_matr[j][i]**2
+        benchmark_ks.append(bench_k_n/bench_k_d)
+        if benchmarks[i].get("scale_factor") is not None:
+            k_n += bench_k_n/bench_k_d*benchmarks[i]["scale_factor"]
+            k_d += bench_k_n**2/bench_k_d**2
+    overall_k = k_n/k_d
+    solve_res = v[-1,:]*overall_k*100
+    return ({model:res for model,res in zip(models, solve_res)}, [benchmark_k*overall_k for benchmark_k in benchmark_ks])
 
 def read_all_benches(filter):
     with open('data_list.json', 'r') as f:
         data_list = json.load(f)
     sources = []
-    for filename in data_list[filter["class"]]:
-        with open(filename, 'r') as f:
+    for file in data_list[filter["class"]]:
+        with open(file["path"], 'r') as f:
             sources.append(json.load(f))
+        for bench, sf in zip(sources[-1]["benchmarks"], file["scale_factors"]):
+            bench["scale_factor"] = sf
     benchmarks = [bench for source in sources if source.get("class") == filter["class"] for bench in source["benchmarks"]]
     benchmarks = [bench for bench in benchmarks if all(bench.get(key) == value for key,value in filter["subfilters"].items())]
     return benchmarks
 
-benchmarks = read_all_benches({"class":"graphics","subfilters":{"type":"gaming-average"}})
-ranking = rank_relative(benchmarks, "RTX 5070")
-print(sorted(ranking.items(), key=lambda x: x[1], reverse=True))
+def update_data_list():
+    with open('data_list.json', 'r') as f:
+        old_data_list = json.load(f)
+    new_data_list = {}
+    for file in os.listdir("data"):
+        if file.endswith(".json"):
+            with open(f"data/{file}", 'r') as f:
+                data = json.load(f)
+            if new_data_list.get(data["class"], None) is None:
+                new_data_list[data["class"]] = []
+            new_data_list[data["class"]] += [{"path":f"data/{file}", "scale_factors":[]}]
+    for class_name in new_data_list.keys():
+        file_scale_factors = {file["path"]:file["scale_factors"] for file in old_data_list.get(class_name, [])}
+        benches = []
+        n_benches = []
+        for file in new_data_list[class_name]:
+            with open(file["path"], 'r') as f:
+                data = json.load(f)
+            sf = file_scale_factors.get(file["path"], [])
+            n_benches.append(len(data["benchmarks"]))
+            for i in range(len(data["benchmarks"])):
+                benches.append(data["benchmarks"][i])
+                if i < len(sf):
+                    benches[-1]["scale_factor"] = sf[i]
+        scores, bench_ks = rank_relative(benches)
+        cumn = 0
+        for i, n in enumerate(n_benches):
+            new_data_list[class_name][i]["scale_factors"] = bench_ks[cumn:cumn+n]
+            cumn += n
+    with open('data_list.json', 'w') as f:
+        json.dump(new_data_list, f, indent=4)
+
+benchmarks = read_all_benches({"class":"graphics","subfilters":{}})
+scores, bench_ks = rank_relative(benchmarks)
+for model, score in sorted(scores.items(), key=lambda x: x[1], reverse=True):
+    print(f"{model:<30} {score:6.0f}")
+
+#update_data_list()
